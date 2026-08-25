b47e58c40c53e47185878a383c17b7857951ded9
nicole <nicole_b_silva@estudante.sesisenai.org.br>
2026-08-25 10:14:43 -0300

    estrutura feita

---
+++ infra/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "pet_shop";
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conexao->connect_error) {
+    die("Erro na conexão com o banco: " . $conexao->connect_error);
+};
+
+$conexao->set_charset("utf8mb4");
+
+?>
