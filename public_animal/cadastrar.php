d33338ee3cd874c706d94488054db52ab4eea1cb
nicole <nicole_b_silva@estudante.sesisenai.org.br>
2026-08-25 11:13:57 -0300

    add-  cadastros

--- public_animal/cadastrar.php
+++ public_animal/cadastrar.php
@@ -3,9 +3,11 @@
 include "../infra/conexao.php";
 
 $nome = $_POST["nome"];
-$email = $_POST["email"];
+$tipo = $_POST["tipo"];
+$raca = $_POST["raca"];
+$idade = $_POST["idade"];
 
-$sql = "INSERT INTO usuarios (nome,email) VALUES ('$nome','$email')";
+$sql = "INSERT INTO animal (nome,tipo,raca,idade) VALUES ('$nome','$tipo','$raca','$idade')";
 
 mysqli_query($conexao, $sql);
 
@@ -18,22 +20,32 @@ header("Location: ../index.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário</title>
+    <title>Cadastro de animal</title>
 </head>
 
 <body>
 
-    <h1>Cadastro de Usuário</h1>
+    <h1>Cadastro de animal</h1>
 
-    <form action="cadastrar_usuario.php" method="POST">
+    <form action="cadastrar_animal.php" method="POST">
 
         <label>Nome:</label>
         <input type="text" name="nome" required>
 
         <br><br>
 
-        <label>E-mail:</label>
-        <input type="email" name="email" required>
+        <label>Tipo:</label>
+        <input type="text" name="tipo" required>
+
+        <br><br>
+
+        <label>Raça:</label>
+        <input type="text" name="raca" required>
+
+        <br><br>
+
+        <label>Idade:</label>
+        <input type="number" name="idade" required>
 
         <br><br>
 
